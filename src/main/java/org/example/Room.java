b09953d84b9ec16c976d36521bffeaefcb18cf89
TheOriginalJiozx <omal0001@stud.kea.dk>
2024-03-07 13:48:01 +0100

    Opdateret

--- src/main/java/org/example/Room.java
+++ src/main/java/org/example/Room.java
@@ -4,6 +4,11 @@ public class Room {
     private Room currentRoom;
     private String name;
     private String description;
+    private Room northRoom;
+    private Room southRoom;
+    private Room eastRoom;
+    private Room westRoom;
+
     public Room(String name, String description) {
         this.name = name;
         this.description = description;
@@ -17,6 +22,38 @@ public class Room {
         return description;
     }
 
+    public void setNorthRoom(Room northRoom) {
+        this.northRoom = northRoom;
+    }
+
+    public void setSouthRoom(Room southRoom) {
+        this.southRoom = southRoom;
+    }
+
+    public void setEastRoom(Room eastRoom) {
+        this.eastRoom = eastRoom;
+    }
+
+    public void setWestRoom(Room westRoom) {
+        this.westRoom = westRoom;
+    }
+
+    public Room getNorthRoom() {
+        return northRoom;
+    }
+
+    public Room getSouthRoom() {
+        return southRoom;
+    }
+
+    public Room getEastRoom() {
+        return eastRoom;
+    }
+
+    public Room getWestRoom() {
+        return westRoom;
+    }
+
     public void helpUser() {
         System.out.println("You are in room: " + getName());
         System.out.println("List of commands: ");
@@ -25,5 +62,20 @@ public class Room {
 
     public void lookAround() {
         System.out.println("Room description: " + getDescription());
+    }
+
+    public Room getRoom(Direction direction) {
+        switch (direction) {
+            case NORTH:
+                return northRoom;
+            case SOUTH:
+                return southRoom;
+            case EAST:
+                return eastRoom;
+            case WEST:
+                return westRoom;
+            default:
+                return null;
+        }
     }
 }
