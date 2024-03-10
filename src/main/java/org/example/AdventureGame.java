888be76b6cc90f37014d40dbab1597dc7c986e5b
TheOriginalJiozx <omal0001@stud.kea.dk>
2024-03-10 10:22:31 +0100

    Merge remote-tracking branch 'origin/main'

--- src/main/java/org/example/AdventureGame.java
+++ src/main/java/org/example/AdventureGame.java
@@ -1,9 +1,9 @@
 package org.example;
-public class AdventureGame {
-    public static void main(String[] args) {
-        Adventure adventure = new Adventure();
-        UserInterface ui = new UserInterface(adventure);
-        ui.startProgram();
 
+public class AdventureGame {
+
+    public static void main(String[] args) {
+        UserInterface ui = new UserInterface();
+        ui.startProgram();
     }
 }
