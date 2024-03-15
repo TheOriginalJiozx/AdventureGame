09a48646f992d80cfd1efc89f9dfe66895794cfe
TheOriginalJiozx <omal0001@stud.kea.dk>
2024-03-15 22:56:44 +0100

    Merge remote-tracking branch 'origin/main'

---
+++ src/main/java/org/example/Enemy.java
@@ -0,0 +1,36 @@
+package org.example;
+
+public class Enemy {
+    private String name;
+    private int health;
+    private int damage;
+
+    public Enemy(String name, int health, int damage) {
+        this.name = name;
+        this.health = health;
+        this.damage = damage;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void takeDamage(int amount) {
+        health -= amount;
+        if (health < 0) {
+            health = 0;
+        }
+    }
+
+    public boolean isDefeated() {
+        return health <= 0;
+    }
+}
